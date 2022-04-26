3d0b1001a3ed977ca7068dc456d2bee7d072f0c3
kbmhansungb <56149613+kbmhansungb@users.noreply.github.com>
2022-04-26 16:30:30 +0900

    로깅을 위해서 CharacterAnimation로그를 추가함.

--- Source/CharacterAnimation/CharacterAnimation.cpp
+++ Source/CharacterAnimation/CharacterAnimation.cpp
@@ -3,5 +3,6 @@
 #include "CharacterAnimation.h"
 #include "Modules/ModuleManager.h"
 
+DEFINE_LOG_CATEGORY(CharacterAnimation);
 IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CharacterAnimation, "CharacterAnimation" );
  
