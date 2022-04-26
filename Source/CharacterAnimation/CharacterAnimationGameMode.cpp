8c18a2e1dc10843c5a38f1af5754cef5a704ff80
kbmhansungb <56149613+kbmhansungb@users.noreply.github.com>
2022-04-26 20:02:24 +0900

    GameMode DefaultPawnClass변경함

--- Source/CharacterAnimation/CharacterAnimationGameMode.cpp
+++ Source/CharacterAnimation/CharacterAnimationGameMode.cpp
@@ -6,10 +6,5 @@
 
 ACharacterAnimationGameMode::ACharacterAnimationGameMode()
 {
-	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPerson/Blueprints/BP_ThirdPersonCharacter"));
-	if (PlayerPawnBPClass.Class != NULL)
-	{
-		DefaultPawnClass = PlayerPawnBPClass.Class;
-	}
+	DefaultPawnClass = ACharacterAnimationCharacter::StaticClass();
 }
